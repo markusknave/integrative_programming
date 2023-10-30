67d68e5301d2d4f58ba3d6b82d2ef2b3335686cf
markusknave <91238449+markusknave@users.noreply.github.com>
2023-10-30 19:07:00 +0800

    Added Activity5
    Activity5 is just updated from Activity4. However, for each new activity given I will just create new folders

---
+++ IntegAct5/IntegAct4/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+
+Route::get('registration', [AuthController::class, 'register'])->name('register');
+
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+
+Route::get('home', [AuthController::class, 'home']); 
+
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+
+?>
